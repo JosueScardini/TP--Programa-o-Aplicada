9f631745f8634dbfe68f6beab61c56b766ba08bd
josuescardini <82621531+JosueScardini@users.noreply.github.com>
2021-07-04 19:41:45 +0000

    Lista encadeada

--- main.cpp
+++ main.cpp
@@ -1,8 +1,86 @@
 #include <iostream>
+#include <locale.h>
+#include <fstream>
 
+using namespace std;
+struct tCircuito {
+  int Res1;
+  int Res2;
+  int Vt1;
+  tCircuito* proximo;
+};
+
+tCircuito* criaNo (int R1, int R2,int V1) {
+  tCircuito* no = new tCircuito;
+  no -> Res1 = R1;
+  no -> Res2 = R2;
+  no -> Vt1 = V1;
+  no -> proximo = NULL;
+
+
+  return no;
+
+};
+
+struct tLista {
+  tCircuito* primeiro;
+  tCircuito* ultimo;
+  tCircuito* marcador;
+  int tamanho;
+
+};
+void inicializaLista(tLista* pLista){
+  pLista -> primeiro = NULL;
+  pLista -> ultimo = NULL;
+  pLista -> marcador = NULL;
+  pLista -> tamanho = 0;
+}
+
+int obterTamanho (tLista* pLista){
+  return pLista -> tamanho;
+}
+
+bool finalizaLista (tLista* pLista) {
+    return pLista -> marcador == NULL;
+    }
+
+bool listaVazia (tLista* pLista){
+  return pLista -> tamanho == 0;
+}
+bool finalLista (tLista* pLista){
+  return (pLista -> marcador == NULL);
+}
+
+void incluirNoFim (tLista* pLista, int R1, int R2, int V1){
+  tCircuito* no;
+  no = criaNo(R1, R2, V1);
+
+  if (listaVazia(pLista)){
+    pLista -> primeiro = no;
+  }else{
+  pLista -> ultimo -> proximo = no;
+  }
+
+  pLista -> ultimo = no;
+  pLista -> marcador = no;
+  pLista -> tamanho ++;
+  }
+
+ void imprimirLista(tLista* pLista){
+   pLista -> marcador = pLista -> primeiro;
+
+   while (!finalLista(pLista)){
+    int informacao = pLista -> marcador->Res1;
+    cout<< "A informação é: "<<informacao<<endl;
+    pLista -> marcador = pLista -> marcador -> proximo;
+   }
+ }
 int main() {
+
 string resposta;
 int numero = 0;
+
+tCircuito circuito1;
 
 cout<<"Digite 1 para gravar ou 2 para ler um banco de dados"<<endl;
 cin>>resposta;
@@ -13,6 +91,10 @@ if (resposta == "2"){
 
 cout<<"Digite o numero de elementos que deseja armazenar"<<endl;
 cin>>numero;
+
+tLista* circuito2 = new tLista;
+inicializaLista(circuito2);
+
 
 for (int i = 0; i < numero; i++){
    cout<<"Informe o valor de R1"<<endl;
@@ -27,6 +109,8 @@ for (int i = 0; i < numero; i++){
 
   cout<<"Informe o valor de V1"<<endl;
   cin>>circuito1.Vt1;
+
+  incluirNoFim(circuito2, circuito1.Res1, circuito1.Res2, circuito1.Vt1);
 }
 }
 }
