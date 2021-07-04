d42ef1c3611dcf3531ebc9eb0b7922f869bfaf38
josuescardini <82621531+JosueScardini@users.noreply.github.com>
2021-07-04 19:31:55 +0000

    Solicitando informações do usuário

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+string resposta;
+int numero = 0;
+
+cout<<"Digite 1 para gravar ou 2 para ler um banco de dados"<<endl;
+cin>>resposta;
+
+if (resposta == "2"){
+
+}else{
+
+cout<<"Digite o numero de elementos que deseja armazenar"<<endl;
+cin>>numero;
+
+for (int i = 0; i < numero; i++){
+   cout<<"Informe o valor de R1"<<endl;
+  cin>> circuito1.Res1;
+
+  cout<< "-------------------------"<<endl;
+
+  cout<<"Informe o valor de R2"<<endl;
+  cin>>circuito1.Res2;
+
+  cout<< "-------------------------"<<endl;
+
+  cout<<"Informe o valor de V1"<<endl;
+  cin>>circuito1.Vt1;
+}
+}
 }
